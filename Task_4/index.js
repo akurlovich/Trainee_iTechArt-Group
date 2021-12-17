b77585dd17dff6b55ca0a467c87d59eadb8f9ce2
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-12-17 12:22:26 +0300

    init: task 4 JavaScript

---
+++ Task_4/index.js
@@ -0,0 +1,5 @@
+import Arrays from './module/arrays.js';
+const narr = new Arrays('Ivan');
+narr.logName();
+
+console.log('import module')
---
+++ Task_4/module/arrays.js
@@ -0,0 +1,10 @@
+export default class Arrays {
+  constructor(obj) {
+    this.obj = obj;
+  }
+
+  logName() {
+    console.log(this.obj)
+  }
+}
+
