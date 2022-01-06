212c1eeaa676e03b034f761964b218f817afc1f6
akurlovich <71250705+akurlovich@users.noreply.github.com>
2022-01-06 16:54:43 +0300

    feat: icons for weather

--- Task_5_React_Weather_App/react-app/src/components/WeatherItem/weatherItem.jsx
+++ Task_5_React_Weather_App/react-app/src/components/WeatherItem/weatherItem.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from './WeatherItem.module.css';
 
 function WeatherItem() {
+  const a = '01'
+  const img1 = `/icons/${a}-s.png`
   return (
     <div className={classes.weather__item}>
       <h2 className={classes.item__title}>Data</h2>
       <div className={classes.item__block}>
-        <div className={classes.item__image}></div>
+        <img src={img1} className={classes.item__image}/>
         <div className={classes.item__info}>
           <div>tempreture</div>
           <div>Osadki</div>
