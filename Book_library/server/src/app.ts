1785f91d06bb9f08ce11c716ba69adbb2a38d0cc
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-01-28 15:33:10 +0300

    feat: connect to MongoDB

--- Book_library/server/src/app.ts
+++ Book_library/server/src/app.ts
@@ -1,17 +1,56 @@
 import express from 'express';
+import { model, Schema, Model, Document, connect } from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
 
 
-app.use('/', (req, res, next) => {
-  if (req.originalUrl === '/') {
-    res.send('Service is running!');
-    return;
-  }
-  next();
+// app.use('/', (req, res, next) => {
+//   if (req.originalUrl === '/') {
+//     res.send('Service is running!');
+//     return;
+//   }
+//   next();
+// });
+
+interface IBook extends Document {
+  title: string;
+  author: string;
+  year: string;
+}
+
+const BookSchema: Schema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  year: { type: String, required: true }
 });
+
+const Book: Model<IBook> = model('User', BookSchema);
+
+
+async function main() {
+  connect('mongodb+srv://node12345:node12345@cluster0.qfiok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: tr
+  }, 
+  () => console.log('Connect to DB')
+  );
+  // const book: IBook = await Book.create({
+  //   title: 'Harry Potter',
+  //   author: 'Bill',
+  //   year: '2001'
+  // });
+
+  // console.log('Done', book.title);
+  const books: Array<IBook> = await Book.find();
+  console.log('Show books', books);
+};
+
+main().catch(err => console.log(err));
+
+
 
 
 export default app;
