55bcebe40d1d4a350e9e58d1406355edecaafdc4
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-01-28 14:12:15 +0300

    feat: basic express server

---
+++ Book_library/server/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/', (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!');
+    return;
+  }
+  next();
+});
+
+
+export default app;
