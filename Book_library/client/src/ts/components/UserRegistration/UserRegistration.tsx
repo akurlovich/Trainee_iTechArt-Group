62f905cba113ce5e8b6ab306ec9b3c032ae84798
akurlovich <71250705+akurlovich@users.noreply.github.com>
2022-02-02 23:20:48 +0300

    feat: validation form data

--- Book_library/client/src/ts/components/UserRegistration/UserRegistration.tsx
+++ Book_library/client/src/ts/components/UserRegistration/UserRegistration.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FormInput } from '../UI/FormInput/FormInput';
 import './userregistration.scss';
@@ -7,10 +7,23 @@ const UserRegistrationInner: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirPassword, setConfirPassword] = useState('');
-  const [buttonSubmit, setButtonSubmit] = useState(true);
+  const [buttonSubmit, setButtonSubmit] = useState(false);
   const handlerChange = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+  };
+  const validFormData = () => {
+    const emailValid = /^\S+@\S+\.\S+$/.test(email);
+    const passValid = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{6,20}$/.test(password);
+    const pasConfirm = (password === confirPassword);
+    if (passValid && pasConfirm && emailValid) {
+      setButtonSubmit(prev => true)
+    }
+
   }
+  useEffect(() => {
+    validFormData();
+  }, [password, confirPassword]);
+  
 
   return (
     <div className='registration'>
@@ -25,11 +38,11 @@ const UserRegistrationInner: FC = () => {
           <FormInput
             placeholder='Email address'
             name='email'
-            type='email'
+            type='text'
             errorMessage='Not valid email!'
             setData={setEmail}
             required={true}
-            // pattern='^[A-Za-z0-9]{3,16}$'
+            pattern='^\S+@\S+\.\S+$'
           />
           <FormInput
             placeholder='Password'
@@ -49,32 +62,11 @@ const UserRegistrationInner: FC = () => {
             required={true}
             pattern={password}
           />
-          {/* <div className="registration__form__inputs-block">
-            <div className="registration__form__input">
-              <input className='registration__input__text' type="email" name="" id="" />
-              <label className='registration__input__label' htmlFor="">
-                Email Address
-              </label>
-            </div>
-            <div className="registration__form__input">
-              <input className='registration__input__text' type="password" name="" id="" />
-              <label className='registration__input__label' htmlFor="">
-                Password
-              </label>
-            </div>
-            <div className="registration__form__input">
-              <input className='registration__input__text' type="password" name="" id="" />
-              <label className='registration__input__label' htmlFor="">
-                Confirm password
-              </label>
-            </div>
-          </div> */}
           <div className="registration__form__button">
             <input
-              className={!buttonSubmit ? "registration__form__button_send" : "registration__form__button_send active"}
+              className={buttonSubmit ? "registration__form__button_send active" : "registration__form__button_send"}
               type="submit"
               value="Sing in"
-              // disabled={true}
             />
           </div>
         </form>
