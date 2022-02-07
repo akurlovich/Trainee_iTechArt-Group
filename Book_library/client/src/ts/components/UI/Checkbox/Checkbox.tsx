ab4eabbdd08fd10b8fed1683efad149fc885e641
akurlovich <71250705+akurlovich@users.noreply.github.com>
2022-02-08 00:33:54 +0300

    feat: custom checkbox style

--- Book_library/client/src/ts/components/UI/Checkbox/Checkbox.tsx
+++ Book_library/client/src/ts/components/UI/Checkbox/Checkbox.tsx
@@ -4,9 +4,9 @@ import './checkbox.scss';
 const CheckboxInner: FC = () => {
   return (
     <div className="checkbox">
-      <input className='checkbox__input' type="checkbox" name="" id="" />
+      <input className='checkbox__input' type="checkbox" name="checkbox__input" id="checkbox__input" />
       <label className='checkbox__label' htmlFor="checkbox__input">Genre</label>
-      <img src="./assets/check-solid.svg" alt="" />
+      <img className='checkbox__image' src="./assets/check-solid.svg" alt="" />
     </div>
   )
 };
