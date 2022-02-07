d65a2d83e5f5d2aac5cb490bcecea42538f8283c
akurlovich <71250705+akurlovich@users.noreply.github.com>
2022-02-07 21:38:30 +0300

    feat: bookblock layout styles

--- Book_library/client/src/ts/components/AllBooks/AllBooks.tsx
+++ Book_library/client/src/ts/components/AllBooks/AllBooks.tsx
@@ -5,10 +5,13 @@ import './allbooks.scss';
 const AllBooksInner: FC = () => {
   return (
     <div className='allbooks'>
+      <div className="allbooks__title">
+        Most popular:
+      </div>
       <div className="allbooks__container">
-        <BookBlock/>
-        <BookBlock/>
-        <BookBlock/>
+        <BookBlock bgColor='#405F71'/>
+        <BookBlock bgColor='#563E70'/>
+        <BookBlock bgColor='#733F55'/>
       </div>
     </div>
   );
--- Book_library/client/src/ts/components/BookBlock/BookBlock.tsx
+++ Book_library/client/src/ts/components/BookBlock/BookBlock.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
 import './bookblock.scss';
 
-const BookBlockItem: FC = () => {
+interface IBooKBlock {
+  bgColor?: string,
+}
+
+const BookBlockItem: FC<IBooKBlock> = ({bgColor}) => {
+  const style = {
+    background: bgColor,
+  }
   return (
     <div className="bookblock">
-      <div className="bookblock__container">
+      <div className="bookblock__container" style={style}>
         <div className="bookblock__cover">
           <div className="bookblock__cover_block">
             <img className='bookblock__cover_image' src="./assets/you_dont_know_JS.jpg" alt="" />
@@ -18,8 +25,13 @@ const BookBlockItem: FC = () => {
           <div className="bookblock__discription">
             With the "You Don’t Know JS" book series, you'll get a more complete understanding of JavaScript, including trickier parts of the language that many experienced JavaScript programmers simply avoid. The series' first book, Up & Going, provides the necessary background for those of you with limited programming experience. By learning the basic building blocks of programming, as well as JavaScript's core mechanisms, you'll be prepared to dive into the other, more in-depth books in the series--and be well on your way toward true JavaScript.
           </div>
-          <div className="bookblock__author">
-            Kyle Simpson
+          <div className="bookblock__info">
+            <div className="bookblock__info_author">
+              Kyle Simpson
+            </div>
+            <div className="bookblock__info_year">
+              2015
+            </div>
           </div>
           <div className="bookblock__button">
             More
