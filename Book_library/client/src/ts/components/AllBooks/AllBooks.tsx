9043b755463e0ba0a634ce6a1901581b2d4e0aa6
akurlovich <71250705+akurlovich@users.noreply.github.com>
2022-02-07 21:47:45 +0300

    feat: search bar layout

--- Book_library/client/src/ts/components/AllBooks/AllBooks.tsx
+++ Book_library/client/src/ts/components/AllBooks/AllBooks.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { BookBlock } from '../BookBlock/BookBlock';
+import { SearchBar } from '../UI/SearchBar/SearchBar';
 import './allbooks.scss';
 
 const AllBooksInner: FC = () => {
@@ -12,6 +13,9 @@ const AllBooksInner: FC = () => {
         <BookBlock bgColor='#405F71'/>
         <BookBlock bgColor='#563E70'/>
         <BookBlock bgColor='#733F55'/>
+      </div>
+      <div className="allbooks__search-bar">
+        <SearchBar/>
       </div>
     </div>
   );
