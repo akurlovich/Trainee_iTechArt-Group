8c15fcba00a37dd1fa53b4682891d1d8c334de3c
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-03-01 11:22:29 +0300

    fix: user profile layout

--- Book_library/client/src/ts/components/UserProfile/UserProfile.tsx
+++ Book_library/client/src/ts/components/UserProfile/UserProfile.tsx
@@ -17,19 +17,19 @@ const UserProfileInner: FC = () => {
   const {userID} = useParams();
   const navigate = useNavigate();
   const location = useLocation();
-  const [imageSrc, setImageSrc] = useState('./assets/book-1.png');
+  const [imageSrc, setImageSrc] = useState('');
+  const [isNewImage, setIsNewImage] = useState(false)
   
   const imageHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file: File = (event.target.files as FileList)[0];
     const urlImage = await base64(file);
-    if (urlImage) {
-      setImageSrc(urlImage as string)
-    }
+    urlImage && setImageSrc(urlImage as string);
+    (urlImage !== imageSrc) && setIsNewImage(true);
   };
 
   const changeImageHandler = () => {
     dispatch(updateUserProfileImage({id: user.id, profileImage: imageSrc}))
-  }
+  };
 
   useEffect(() => {
     (async () => {
@@ -54,23 +54,29 @@ const UserProfileInner: FC = () => {
             <input
               onChange={imageHandler}
               className='inputs__files_display' type="file" name="label_for_file" id="label_for_file" />
-            <label className='inputs__files__label' htmlFor="label_for_file">Select file</label>
-            <button
-              onClick={changeImageHandler}
-              className="profile__info__avatar_button">Change image</button>
+            <div className="profile__image-block">
+              <label className='profile__files-label' htmlFor="label_for_file">Select file</label>
+              {isNewImage && 
+                <button
+                  onClick={changeImageHandler}
+                  className="profile__info__avatar_button">
+                  Save image
+                </button>
+              }
+            </div>
           </div>
           <div className="profile__info__email">
             <div className="profile__info__email_title">User email:</div>
             <div className="profile__info__email_adress">{user.email}</div>
           </div>
-          <div className="profile__info__password">
+          {/* <div className="profile__info__password">
             <div className="profile__info__password__block">
               <input className="profile__info__password_input" type="password" placeholder='Curent password'/>
               <input className="profile__info__password_input" type="password" placeholder='New password'/>
               <input className="profile__info__password_input" type="password" placeholder='Confirm new password'/>
             </div>
             <button className="profile__info__password_button">Change password</button>
-          </div>
+          </div> */}
         </div>
         <div className="profile__books">
           <div className="profile__title">Booked books:</div>
