38c1af94e91b98a598622f8750eae61aaa039f48
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-03-09 09:20:08 +0300

    fix: book item layout

--- Book_library/client/src/ts/components/BookItem/BookItemAdmin/BookItemAdmin.tsx
+++ Book_library/client/src/ts/components/BookItem/BookItemAdmin/BookItemAdmin.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
-import { useAppSelector } from '../../../hooks/redux';
+import React, { FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import { getBookByID } from '../../../store/reducers/BookReducer/BookActionCreatores';
 import { Booking } from '../../Booking/Booking';
 import { Loader } from '../../UI/Loader/Loader';
 import { BookItemLog } from '../BookItemLog/BookItemLog';
@@ -10,13 +11,32 @@ interface IProps {
 }
 
 const BookItemAdminInner:FC<IProps> = ({bookID}) => {
+  const { book } = useAppSelector(state => state.bookReducer);
   const { isLoading } = useAppSelector(state => state.bookedReducer);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+        await dispatch(getBookByID(bookID));
+    })()
+  }, []);
+
   return (
     <>
       {isLoading && <Loader/>}
       <div className='bookitemadmin'>
-        <Booking isSearch={false} bookAdminID={bookID}/>
-        <BookItemLog/>
+        <div className="bookitemadmin__title">
+          <div className="bookitemadmin__name">
+            {book.title}
+          </div>
+          <div className="bookitemadmin__amount">
+            {`Amount: ${book.amount}`}
+          </div>
+        </div>
+        <div className="bookitemadmin__main">
+          <Booking isSearch={false} bookAdminID={bookID}/>
+          <BookItemLog/>
+        </div>
       </div>
     </>
   )
