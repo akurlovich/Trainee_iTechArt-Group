42ecca50415e6ba0d1611b510b6351cbe7b076c5
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-03-16 10:22:18 +0300

    feat: logger for add and cansel booking

--- Book_library/server/src/controllers/booked-controller.ts
+++ Book_library/server/src/controllers/booked-controller.ts
@@ -3,11 +3,16 @@ import bookedService from "../service/booked-service";
 import schedule from 'node-schedule';
 import mailService from "../service/mail-service";
 import Logger from "../logger/log";
+import userService from "../service/user-service";
+import bookService from "../service/book-service";
 
 class BookedController {
   async addBooked(req: Request, res: Response, next: NextFunction) {
     try {
       const newBooked = await bookedService.addBooked(req.body);
+      const book = await bookService.getBookByID(newBooked.bookID.toString())
+      const user = await userService.getUserByID(newBooked.userID.toString())
+      Logger.info(`Booking book ${book.title} by User ${user.email}`, { action: 'booking', book: book.title, user: user.email});
       return res.json(newBooked);
     } catch (error) {
       console.log(error);
@@ -48,7 +53,10 @@ class BookedController {
   async getAllBookeds(req: Request, res: Response, next: NextFunction) {
     try {
       const bookeds = await bookedService.getAllBookeds();
-      Logger.info(`${bookeds[0].userID}`);
+      // logger1.info(`${bookeds[0].userID}`, {...bookeds[0], action: 'register'});
+      // logger1.info(`${bookeds[0].userID}`, { action: 'register'});
+      // Logger.info(`${bookeds[0].userID}`, { action: 'register', title: bookeds[0].bookID});
+      // Logger.info(`${bookeds[0].userID}`, {action: 'register'});
 
       return res.json(bookeds);
     } catch (error) {
@@ -60,6 +68,9 @@ class BookedController {
   async deleteBooked(req: Request<{ id: string }>, res: Response, next: NextFunction) {
     try {
       const booked = await bookedService.deleteBooked(req.params.id);
+      const book = await bookService.getBookByID(booked.bookID.toString())
+      const user = await userService.getUserByID(booked.userID.toString())
+      Logger.info(`Cansel booking book ${book.title} by User ${user.email}`, { action: 'cansel booking', book: book.title, user: user.email});
       return res.json(booked);
     } catch (error) {
       console.log(error);
--- Book_library/server/src/logger/log.ts
+++ Book_library/server/src/logger/log.ts
@@ -3,72 +3,35 @@ import path from 'path';
 
 const LOGS_DIR = path.join(__dirname, '../logs');
 
-const levels = {
-  error: 0,
-  warn: 1,
-  info: 2,
-  http: 3,
-  debug: 4,
-};
-
-const level = () => {
-  const env = process.env['NODE_ENV'] || 'development';
-  const isDevelopment = env === 'development';
-  return isDevelopment ? 'debug' : 'warn';
-};
-
-const colors = {
-  error: 'red',
-  warn: 'yellow',
-  info: 'green',
-  http: 'magenta',
-  debug: 'white',
-};
-
-const format = winston.format.combine(
-  winston.format.timestamp({ format: 'MM-DD HH:mm:ss' }),
-  // winston.format.colorize({ all: true }),
-  // winston.format.printf((error) => `${error['timestamp']}: ${error.message}`)
-  // winston.format.printf(({level, message, timestamp}) => JSON.stringify(`lev = ${level}, mes = ${message}, time = ${timestamp}`)),
-  winston.format.printf(({message}) => JSON.stringify(`${message}`)),
-  // winston.format.json(),
-  );
-
-const transports = [
-  new winston.transports.Console({handleExceptions: true}),
-  new winston.transports.File({
-    filename: `${LOGS_DIR}/error.log`,
-    level: 'error',
-    handleExceptions: true,
-    format: winston.format.json(),
-  }),
-  new winston.transports.File({
-    filename: `${LOGS_DIR}/all.log`,
-    format: winston.format.json(),
-  }),
-  new winston.transports.File({
-    filename: `${LOGS_DIR}/http.log`,
-    format: winston.format.json(),
-    level: 'http',
-  }),
-  new winston.transports.File({
-    filename: `${LOGS_DIR}/booking.log`,
-    // format: winston.format.combine(
-    //   winston.format.timestamp(),
-    //   winston.format.json(),
-    // ),
-    // format: winston.format.printf(({level, message, timestamp}) => `lev = ${level}, mes = ${message}, time = ${timestamp}`),
-    format: format,
-    level: 'info',
-  }),
-];
+// const logFormat = winston.format.printf(info => `${info['timestamp']} ${info.level} [${info['label']}]: ${info.message}`);
 
 const Logger = winston.createLogger({
-  // level: level(),
-  // levels,
-  // format,
-  transports,
-  exitOnError: false,
-});
+  // level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  level: 'info',
+  format: winston.format.combine(
+    // winston.format.label({ label: path.basename(process.mainModule.filename) }),
+    // winston.format.label({ label: 'hi all' }),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    // Format the metadata object
+    winston.format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] })
+  ),
+  transports: [
+    // new winston.transports.Console({
+    //   format: winston.format.combine(
+    //     winston.format.colorize(),
+    //     logFormat
+    //   )
+    // }),
+    new winston.transports.File({
+      filename: `${LOGS_DIR}/booking.log`,
+      format: winston.format.combine(
+     
+        // winston.format.splat(),
+        winston.format.json()
+      )
+    })
+  ],
+  exitOnError: false
+})
 
 export default Logger;
