159af9626f0168fab903bc13e592e2c1d979eabf
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-04-11 11:02:00 +0300

    feat: 404 page

--- Book_library/client/src/ts/components/PageNotFound/PageNotFound.tsx
+++ Book_library/client/src/ts/components/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './pagenotfound.scss';
 
 const PageNotFoundInner: FC = () => {
+  const navigate = useNavigate();
+  const buttonHandler = () => {
+    navigate('/');
+  };
   return (
     <div className='pagenotfound'>
-      404
+      <div className="pagenotfound__text">
+        Sorry, page not found!
+      </div>
+      <button onClick={buttonHandler} className="pagenotfound__button">Go home</button>
     </div>
   );
 };
